e801582199caa6a8a75a500ed78b949261078a9a
Bagitbek <bagitbek1@gmail.com>
2022-11-15 20:52:10 +0600

    Small submitting form example

---
+++ src/main/java/com/example/simpleformsubmission/SimpleFormSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleformsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFormSubmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleFormSubmissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simpleformsubmission/SimpleFormSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleformsubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFormSubmissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
